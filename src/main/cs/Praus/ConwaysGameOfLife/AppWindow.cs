f7aca46032ddecad27d03303b02fecd6690523e2
Jan Tajovský <jan.tajov@gmail.com>
2016-02-27 09:22:18 +0100

    Add Log4net and configure it

--- src/main/cs/Praus/ConwaysGameOfLife/AppWindow.cs
+++ src/main/cs/Praus/ConwaysGameOfLife/AppWindow.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Praus.ConwaysGameOfLife.Utils;
+
 
 namespace Praus.ConwaysGameOfLife {
-    public partial class AppWindow : Form {
+    public partial class AppWindow : Form, ILogUtils {
         public AppWindow() {
             InitializeComponent();
+            this.GetLogger().Debug("kafsjsla");
+            this.GetLogger().Info("kafsjsla");
+            this.GetLogger().Warn("kafsjsla");
+            this.GetLogger().Error("kafsjsla");
+            this.GetLogger().Fatal("kafsjsla");
         }
     }
 }
---
+++ src/main/cs/Praus/ConwaysGameOfLife/Utils/LogUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using log4net;
+
+namespace Praus.ConwaysGameOfLife.Utils {
+    public static class LogUtils {
+        public static ILog GetLogger(this ILogUtils cls) {
+            return LogManager.GetLogger(cls.GetType());
+        }
+    }
+    /// <summary>
+    /// ILogUtils interface.
+    /// Enables the implementing class to use the LogUtis extension
+    /// methods. 
+    /// </summary>
+    public interface ILogUtils {
+        // Empty interface just for enabling LogUtils
+    }
+}
+
