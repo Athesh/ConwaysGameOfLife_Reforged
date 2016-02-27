99ab8982a10c8321f7e59c7c5e21ca85c72e1d6f
Jan Tajovský <jan.tajov@gmail.com>
2016-02-27 09:23:32 +0100

    Add node interface and base abstract

---
+++ src/main/cs/Praus/ConwaysGameOfLife/Model/QTree/INode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Praus.ConwaysGameOfLife.Model.QTree {
+    /// <summary>
+    /// QTree node interface.
+    /// </summary>
+    public interface INode {
+        /// <summary>
+        /// Get the north west node(the top right).
+        /// </summary>
+        /// <value>North west node</value>
+        INode NorthWest { get; }
+        /// <summary>
+        /// Get the north east node(the top left).
+        /// </summary>
+        /// <value>North east node</value>
+        INode NorthEast { get; }
+        /// <summary>
+        /// Get the south west node(the bottom right).
+        /// </summary>
+        /// <value>South west node</value>
+        INode SouthWest { get; }
+        /// <summary>
+        /// Get the sout east node(the bottom left).
+        /// </summary>
+        /// <value>South east node</value>
+        INode SouthEast { get; }
+        /// <summary>
+        /// Get the distance to root node.
+        /// </summary>
+        /// <value>Distance to the root node</value>
+        uint RootDistance { get; }
+        /// <summary>
+        /// Get the population in this node.
+        /// </summary>
+        /// <value>Node population</value>
+        ulong Population { get; }
+        /// <summary>
+        /// Get value indicating whether node is "Alive" or "Dead" in case
+        /// it is the "Leaf" node. Otherwise throw an exception.
+        /// </summary>
+        /// <value><c>true</c> if this instance is alive; otherwise, <c>false</c>.</value>
+        bool IsAlive { get; }
+        /// <summary>
+        /// Get value indicating whether node is leaf.
+        /// </summary>
+        /// <value><c>true</c> if this instance is leaf; otherwise, <c>false</c>.</value>
+        bool IsLeaf { get; }
+    }
+}
+
---
+++ src/main/cs/Praus/ConwaysGameOfLife/Utils/LinqUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Praus.ConwaysGameOfLife.Utils {
+    public static class LinqUtils {
+        public static ulong Sum(this IEnumerable<ulong> ulongs) {
+            ulong sum = 0;
+            foreach (var ul in ulongs) {
+                sum += ul;
+            }
+            return sum;
+        }
+    }
+}
+
