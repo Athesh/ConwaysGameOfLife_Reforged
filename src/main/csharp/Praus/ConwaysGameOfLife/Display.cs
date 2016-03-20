2b197fe2e83fc6106ddce1e97ba99d4caa2934d1
Jan Tajovský <jan.tajov@gmail.com>
2016-03-20 18:53:09 +0100

    Zooming

--- src/main/csharp/Praus/ConwaysGameOfLife/Display.cs
+++ src/main/csharp/Praus/ConwaysGameOfLife/Display.cs
@@ -9,7 +9,7 @@ namespace Praus.ConwaysGameOfLife {
         public Action<int, int> SetCell { get; set; } 
         public int Rows { get; set; } = 35;
         public int Cols { get; set; } = 46;
-        public int SquareSize { get; set; } = 9;
+        public int SquareSize { get; set; } = 15;
 
         public Display() {
             SetStyle(ControlStyles.AllPaintingInWmPaint, true);
@@ -20,6 +20,22 @@ namespace Praus.ConwaysGameOfLife {
         protected override void OnMouseClick(MouseEventArgs e) {
             SetCell?.Invoke(e.X / SquareSize - Cols, e.Y / SquareSize - Rows);
             this.Invalidate();
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e) {
+            var d = SquareSize + (e.Delta > 0 ? 1 : -1);
+            if (d > 15) {
+                SquareSize = 15;
+            } else if (d < 3) {
+                SquareSize = 3;
+            } else {
+                SquareSize = d;
+            }
+            this.Invalidate();
+        }
+
+        protected override void OnMouseHover(EventArgs e) {
+            this.Focus();
         }
 
         protected override void OnPaint(PaintEventArgs e) {
